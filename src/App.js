2ad6619c495122fd3e8fb1e268bf40ca9c8f415d
Naimur Rahman <naimur.rahman@suffixit.com>
2023-12-14 18:01:08 +0600

    Login Page Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import LoginScreen from "./screen/LoginScreen";
 import MenuScreen from "./screen/MenuScreen";
 
 function App() {
   return (
     <>
-      <MenuScreen />
+      <LoginScreen />
     </>
   );
 }
